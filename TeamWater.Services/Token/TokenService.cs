fb0a34b41bbf61fc8bcbbf866e441d550eb979b6
Darneisha Miller <darneisham20@gmail.com>
2023-01-05 21:40:44 -0500

    Tokens Started: TokenRequest,TokenResponse,ITokenService, etc.

--- TeamWater.Services/Token/TokenService.cs
+++ TeamWater.Services/Token/TokenService.cs
@@ -2,11 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TeamWater.Data.Entities;
+using TeamWater.Models.Token;
 
 namespace TeamWater.Services.Token
 {
-    public class TokenService :ITokenService
+    public class TokenService : ITokenService
     {
-        
+        public async Task<TokenResponse> GetTokenAsync(TokenRequest model)
+        {
+            var UserEntity = await GetValidUserAsync(model);
+            if (userEntity is null)
+                return null;
+
+            return GenerateToken(userEntity);
+        }
+
+        private async Task<UserEntity> GetValidUserAsync(TokenRequest model) 
+        { 
+            var userEntity = await GetValidUserAsync(model);
+            if (userEntity is null)
+                return null;
+            
+            return GenerateToken(userEntity);
+        }
+
+        private TokenResponse GenerateToken(UserEntity entity) { }
     }
 }
--- TeamWater.Services/User/UserService.cs
+++ TeamWater.Services/User/UserService.cs
@@ -4,11 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using TeamWater.Data.Entities;
 using TeamWater.Models.User;
+using TeamWater.Data;
 
 namespace TeamWater.Services.User
 {
+    private readonly ApplicationDbContext _context;
+    public UserService(ApplicationDbContext context) 
+    {
+        _context = context;
+    }
+
+
     public class UserService : IUserService
     {
-        
+        public async Task<bool> RegisterUserAsync(UserRegister model)
+        {
+            var entity = new UserEntity
+            {
+                Email = model.Email,
+                UserName = model.Username,
+                Password = model.Password,
+                DateCreated = DateTime.Now
+            };
+
+            _context.Users.Add(entity);
+            var numberOfChanges = await _context.SaveChangesAsync();
+
+            return numberOfChanges == 1;
+        }
     }
 }
