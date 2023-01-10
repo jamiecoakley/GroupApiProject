02c013ed09eef3d31f69bd468ef8b81e6ca3ee36
jamiecoakley <jamiemcoakley@gmail.com>
2023-01-10 18:30:53 -0500

    Completed user creation functionality by following steps in the modules from ElevenNote. Tested on Swagger, and it's working!

--- TeamWater.WebAPI/Controllers/UserController.cs
+++ TeamWater.WebAPI/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TeamWater.Models.User;
 using TeamWater.Services.User;
 
 namespace TeamWater.WebAPI.Controllers
@@ -16,6 +17,19 @@ namespace TeamWater.WebAPI.Controllers
         public UserController(IUserService service)
         {
             _service = service;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserRegister model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var registerResult = await _service.RegisterUserAsync(model);
+            if (registerResult)
+                return Ok("Congratulations! You have signed up to review shows!");
+
+            return BadRequest("Shucks! Something went wrong. Go complain to the devs and try again later!");
         }
     }
 }
