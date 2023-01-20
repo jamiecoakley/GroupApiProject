3169c1ea8c7e7e4a0965f2e52a6374a56bd87454
Darneisha Miller <darneisham20@gmail.com>
2023-01-19 20:12:04 -0500

    Episode Entity Migration Added

---
+++ TeamWater.Data/Migrations/20230120011052_Updated_EpisodeEntity_With_TvShowIDFK.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamWater.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedEpisodeEntityWithTvShowIDFK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Episodes_TvShows_TvShowEntityId",
+                table: "Episodes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Episodes_TvShowEntityId",
+                table: "Episodes");
+
+            migrationBuilder.DropColumn(
+                name: "TvShowEntityId",
+                table: "Episodes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Episodes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Episodes_TvShowId",
+                table: "Episodes",
+                column: "TvShowId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Episodes_TvShows_TvShowId",
+                table: "Episodes",
+                column: "TvShowId",
+                principalTable: "TvShows",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Episodes_TvShows_TvShowId",
+                table: "Episodes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Episodes_TvShowId",
+                table: "Episodes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Episodes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TvShowEntityId",
+                table: "Episodes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Episodes_TvShowEntityId",
+                table: "Episodes",
+                column: "TvShowEntityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Episodes_TvShows_TvShowEntityId",
+                table: "Episodes",
+                column: "TvShowEntityId",
+                principalTable: "TvShows",
+                principalColumn: "Id");
+        }
+    }
+}
