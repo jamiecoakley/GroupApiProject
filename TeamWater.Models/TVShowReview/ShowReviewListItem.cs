c3e23891ab17a661a53ce10944b324e06a4ba19b
Michael Kinsey <mrkinsey10@gmail.com>
2023-01-21 08:53:46 -0500

    final changes

--- TeamWater.Models/TVShowReview/ShowReviewListItem.cs
+++ TeamWater.Models/TVShowReview/ShowReviewListItem.cs
@@ -8,7 +8,7 @@ namespace TeamWater.Models.TVShowReview
     public class ShowReviewListItem
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
+        // public int UserId { get; set; }
         public string ReviewTitle { get; set; }
         public int ShowRating { get; set; }
         public string ReviewText { get; set; }
